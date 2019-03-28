17d4640a3a8d44fc76683d2887327054b65c5d30
prasad-s-h <prasads367@gmail.com>
2019-03-28 10:01:48 +0530

    sendMessage button issue

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,24 +2,24 @@ console.log('loaded');
 const socket = io();
 
 const form = document.querySelector('form');
-let input = form.querySelector('input');
-let para1 = form.querySelector('#receivedMessage');
-let para2 = form.querySelector('#notification');
-let sendButton = form.querySelector('#sendMsg');
-let shareButton = document.querySelector('#shareLocation');
+let input = form.querySelector('input'); // text box to enter the message
+let para1 = form.querySelector('#receivedMessage'); //first paragraph to display the message entered by user OR Location Details
+let para2 = form.querySelector('#notification'); //second paragraph to display notification about new user join and user leaving
+let sendButton = form.querySelector('#sendMsg'); //a button to send message from text box
+let shareButton = document.querySelector('#shareLocation'); //button to share user location
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     
-    sendButton.setAttribute('disabled', 'disabled');
+    sendButton.setAttribute('disabled', 'disabled'); //disable button to send message
 
-    const textMessage = input.value;
-    console.log('textMessage = ', textMessage);
+    const textMessage = input.value; //temporarily store in other variable
+    console.log('textMessage = ', textMessage); 
 
     socket.emit('clientSentMsg', textMessage, (error) => {
-        sendButton.removeAttribute('disabled');
-        input.value = '';
-        input.focus();
+        sendButton.removeAttribute('disabled'); // ******this does not work****** //
+        input.value = ''; // ******this does not work****** //
+        input.focus(); // ******this does not work****** //
 
         if(error) {
             return para1.textContent = error;
